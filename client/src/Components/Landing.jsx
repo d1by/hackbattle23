84c1513bd09fe205d4c3ad83596437f26ac9bbe5
Harsh Kumar Sinha <hksinha960@gmail.com>
2023-09-20 10:39:14 +0530

    Upadted frontend

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -3,7 +3,7 @@ function Landing() {
   return (
     <div className="landing ">
       <Navbar />
-      <div className="flex pt-16 pb-80 px-40 flex-col items-start text-white text-8xl">
+      <div className="flex pl-32 pt-12 pb-8 flex-col items-start text-white text-8xl">
         <h1 className="py-2">Lorem ipsum</h1>
         <h1 className="py-2">dolor sit amet</h1>
       </div>

49e43cadb43595133a5eff0597ad88e4db56fda2
Harsh Kumar Sinha <hksinha960@gmail.com>
2023-09-21 02:38:47 +0530

    Completed Little integration

--- client/src/Components/Contract.jsx
+++ client/src/Components/Contract.jsx
@@ -19,22 +19,35 @@ function Contract() {
         <div className="pl-8 pt-16">
           <div className="flex flex-col items-start">
             <input
-              className=" px-40 py-12 border-b border-[#D9D9D9] text-5xl text-[#DDFB00] b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
+              className=" px-40 py-6 border-b border-[#D9D9D9] text-5xl text-[#DDFB00] b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
               type="text"
               placeholder="Pogba"
             />
             <input
-              className="px-40 py-12 border-b border-[#D9D9D9] text-5xl text-[#DDFB00]  b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
+              className="px-40 py-6 border-b border-[#D9D9D9] text-5xl text-[#DDFB00]  b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
               type="text"
               placeholder="Al-Hilal"
             />
             <input
-              className="px-5 py-12 border-b border-[#D9D9D9] text-5xl text-[#DDFB00]  b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
+              className="px-5 py-6 border-b border-[#D9D9D9] text-5xl text-[#DDFB00]  b-0 bg-transparent placeholder:text-[#DDFB00] placeholder:text-5xl"
               type="text"
               placeholder="$100,000.00"
             />
           </div>
-          <div></div>
+          <div className="flex pt-5">
+            <div className="">
+              <img src="./images/clause.png" alt="" />
+            </div>
+            <div className="pl-8">
+              <div className="text-[#DDFB00] text-7xl ">TERMS</div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Expedita officia tenetur, vitae commodi quis dicta quaerat non
+                nemo dolorem deleniti at tempora numquam vel obcaecati suscipit
+                sunt sapiente tempore alias.
+              </p>
+            </div>
+          </div>
         </div>
       </form>
     </div>
--- client/src/Components/Moto.jsx
+++ client/src/Components/Moto.jsx
@@ -2,21 +2,17 @@ function Moto() {
   return (
     <div className="bg-[#262626]">
       <div className="flex">
-        <img src="./images/moto-bg.png"></img>
+        <img src="./images/moto-bg.png" alt=""></img>
         <div className="flex flex-col text-white text-left p-12 gap-5 justify-center">
           <p className="moto-font-one">Our Philosophy</p>
           <p className="moto-font-two">WHAT WE DO</p>
           <p className="moto-font-one">
-            Steal raw zucchini off kitchen counter sit in box and please let me
-            outside pouty face yay! wait, it's cold out please let me inside
-            pouty face oh, thank you rub against mommy's leg oh it looks so nice
-            out, please let me outside again the neighbor cat was mean to me
-            please let me back inside for twitch tail in permanent irritation
-            scratch. Pooping rainbow while flying in a toasted bread costume
-            mommy's leg oh it looks so nice out, please let me outside again the
-            neighbor cat was mean to me please let me back inside for twitch
-            tail in permanent irritation scratch. Pooping rainbow while flying
-            in a toasted bread costume
+            We run a contract enforcement system that leverages blockchain
+            technology, ensuring impartial and timely adherence to contract
+            terms by both parties. We also integrate machine learning algorithms
+            to asses and verify the fairness of each clauses to benefit both
+            parties. To ensure losses by either party implicated in
+            breaches of contract.
           </p>
         </div>
       </div>

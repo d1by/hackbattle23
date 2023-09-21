4462fe3bbaf86ca6dfa5eb69d81c56485b689dfe
Aradhye Swarup <aradhye@Aradhyes-MacBook-Air.local>
2023-09-21 10:37:24 +0530

    asap

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
-function Navbar() {
+import {Link} from "react-router-dom";
+
+function Navbar(props) {
   const connectWallet = async () => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
       try {
@@ -49,37 +51,41 @@ function Navbar() {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0  border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
               <li>
-                <a
-                  href="#home"
+              <Link to="/" state={props.state}>
+                <p
                   className="block py-2 pl-3 pr-4 text-xl text-white-500 duration-500 hover:text-2xl  md:bg-transparent md:text-[#FF7A00] md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </p>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#team"
+                <p
                   className="block py-2 pl-3 pr-4 text-xl  text-white hover:text-2xl duration-500  md:hover:bg-transparent md:border-0 md:hover:text-[#FF7A00] md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Team
-                </a>
+                </p>
               </li>
               <li>
-                <a
+              <Link to="/players" state={props.state}>
+                <p
                   href="#players"
                   className="block py-2 pl-3 pr-4 text-xl text-white hover:text-2xl duration-500  md:hover:bg-transparent md:border-0 md:hover:text-[#FF7A00] md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Players
-                </a>
+                </p>
+                </Link>
               </li>
               <li>
-                <a
+                <Link to="/contract" state={props.state}>
+                <p
                   href="./Contract"
                   className="block py-2 pl-3 pr-4 text-xl text-white hover:text-2xl duration-500  md:hover:bg-transparent md:border-0 md:hover:text-[#FF7A00] md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Contracts
-                </a>
+                </p>
+                </Link>
               </li>
               <li>
                 <div className="px-5 ">
